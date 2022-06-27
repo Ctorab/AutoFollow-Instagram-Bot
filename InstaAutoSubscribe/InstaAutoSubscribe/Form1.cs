68bc8698d775c50b3fd1278a9444a1d4e298d232
aSaSnowDev <103963373+aSaSnowDev@users.noreply.github.com>
2022-06-27 19:46:53 +0300

    fix

--- InstaAutoSubscribe/InstaAutoSubscribe/Form1.cs
+++ InstaAutoSubscribe/InstaAutoSubscribe/Form1.cs
@@ -131,7 +131,7 @@ namespace InstaAutoSubscribe
                 await _api.FollowUserAsync(_targetPageFollowers[i].Pk);
                 _currentUserFollowings.Add(_targetPageFollowers[i]);
 
-                Thread.Sleep(int.TryParse(Delay.Text, out int delay) ? delay : 1000);
+                await Task.Delay(int.TryParse(Delay.Text, out int delay) ? delay : 1000);
             }
 
             MessageBox.Show("Finished following!");
